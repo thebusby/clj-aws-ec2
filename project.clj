98689e2d3773a856281b33d0874f67ab6b3f3ffe
Alan Busby <abusby@gracenote.com>
2013-08-29 14:38:06 +0900

    Updated project via new branch

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject clj-aws-ec2 "0.2.1"
+(defproject thebusby/clj-aws-ec2 "0.2.2"
   :description "Clojure Amazon EC2 library."
-  :url "https://github.com/mrowe/clj-aws-ec2"
+  :url "https://github.com/thebusby/clj-aws-ec2"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [com.amazonaws/aws-java-sdk "1.3.23"]]
-  :plugins [[codox "0.6.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.amazonaws/aws-java-sdk "1.5.5"]])
